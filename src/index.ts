ff1cd5f0747f401b24e02c8c3e4f7d3020049fe4
Sekwah <contact@sekwah.com>
2021-10-22 11:40:28 +0100

    feat: Add default export

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
-export { PromiseQueue } from "./PromiseQueue";
+import { PromiseQueue } from "./PromiseQueue";
+
+export { PromiseQueue };
 export { QueuedTaskFactory } from "./QueuedTask";
 export { OptionalFunc, QueuedPromiseFactory, QueuedTaskProps } from "./TaskTypes";
+
+export default PromiseQueue;
