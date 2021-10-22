9083adef3723008a4e18fa79054b5e368391c364
Sekwah <contact@sekwah.com>
2021-10-22 11:30:11 +0100

    refactor: Update project structure

---
+++ src/TaskTypes.ts
@@ -0,0 +1,5 @@
+import {QueuedTask} from "./QueuedTask";
+
+export type OptionalFunc = (value?: any) => void;
+export type QueuedTaskProps = (res: OptionalFunc, rej: OptionalFunc) => void;
+export type QueuedPromiseFactory = (props: QueuedTaskProps) => Promise<any>;
