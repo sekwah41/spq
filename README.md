46b237b26ab9cd7e5779f856621ae2c1527e403b
Sekwah <contact@sekwah.com>
2023-01-11 22:54:24 +0000

    chore: run prettier

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
-spq (Simple Promise Queue) [![Build](https://github.com/sekwah41/spq/actions/workflows/ci.yml/badge.svg)](https://github.com/sekwah41/spq/actions/workflows/ci.yml) [![CodeQL](https://github.com/sekwah41/spq/actions/workflows/codeql-analysis.yml/badge.svg)](https://github.com/sekwah41/spq/actions/workflows/codeql-analysis.yml) [![CII Best Practices](https://bestpractices.coreinfrastructure.org/projects/5476/badge)](https://bestpractices.coreinfrastructure.org/projects/5476)
-================
+# spq (Simple Promise Queue) [![Build](https://github.com/sekwah41/spq/actions/workflows/ci.yml/badge.svg)](https://github.com/sekwah41/spq/actions/workflows/ci.yml) [![CodeQL](https://github.com/sekwah41/spq/actions/workflows/codeql-analysis.yml/badge.svg)](https://github.com/sekwah41/spq/actions/workflows/codeql-analysis.yml) [![CII Best Practices](https://bestpractices.coreinfrastructure.org/projects/5476/badge)](https://bestpractices.coreinfrastructure.org/projects/5476)
 
 This project exists because of an ongoing problem in projects with too many promises resolving at once.
 Rather than rewriting the workflow to work with scheduling library's this is designed to be a drop in replacement.
@@ -8,60 +7,69 @@ After a few lines of configuring you should be able to just replace `new Promise
 You can also provide custom names which will allow you to add the promises to different queues.
 
 # Example
+
 Using promises
+
 ```javascript
 const firstPromise = new Promise((resolve: () => void) => {
-    setTimeout(() => {
-        resolve(2);
-    }, 10);
+  setTimeout(() => {
+    resolve(2);
+  }, 10);
 });
 
 // Imagine if there was an issue with this running at the same time or too many at once. e.g. too many open connections
 const secondPromise = new Promise((resolve: () => void) => {
-    resolve(3);
+  resolve(3);
 });
 
 Promise.all([firstPromise, secondPromise]);
 ```
 
 Converted to using the PromiseQueue
-```javascript
 
-const { PromiseQueue } = require('spq');
+```javascript
+const { PromiseQueue } = require("spq");
 
 const customQueue = new spq.PromiseQueue(1);
 const QueuedPromise = customQueue.QueuedPromise;
 
 const firstPromise = QueuedPromise((resolve: () => void) => {
-    setTimeout(() => {
-        resolve(2);
-    }, 10);
+  setTimeout(() => {
+    resolve(2);
+  }, 10);
 });
 
 // This won't run until the first one is fully resolved as the queue has a size of 1
 const secondPromise = QueuedPromise((resolve: () => void) => {
-    resolve(3);
+  resolve(3);
 });
 
 Promise.all([firstPromise, secondPromise]);
 ```
 
 # PromiseQueue
+
 ## Methods
+
 `pause()` paused the queue but doesnt stop already running promises
 
 `resume()` starts the queue and starts firing tasks.
 
 ## Events
-Do not change _ values directly. The event will not fire and changing to true wont trigger
+
+Do not change \_ values directly. The event will not fire and changing to true wont trigger
 queueing of new tasks.
+
 #### `paused`
+
 Fired when the queue is paused.
 
 #### `resumed`
+
 Fired when the queue is resumed.
 
 #### `finished`
+
 Fired when the last task is finished and no more are queued.
 
 # Contributing
@@ -72,7 +80,7 @@ https://www.conventionalcommits.org/en/v1.0.0/
 
 All new methods or fixes must be covered with unit tests.
 
-
 # Future of this project
+
 This was meant to be a quick projects to help queue code which otherwise overloads systems, if there are any features
 please feel free to open an issue and suggest possible features or ways to interact with this library.
--- src/__tests__/PromiseQueue.test.ts
+++ src/__tests__/PromiseQueue.test.ts
@@ -1,267 +1,251 @@
-import {PromiseQueue} from "../PromiseQueue";
-import {OptionalFunc, QueuedPromiseFactory} from "../TaskTypes";
-import {QueuedTaskFactory} from "../QueuedTask";
+import { PromiseQueue } from "../PromiseQueue";
+import { OptionalFunc, QueuedPromiseFactory } from "../TaskTypes";
+import { QueuedTaskFactory } from "../QueuedTask";
 
-describe('Basic checks on QueuedPromise', () => {
+describe("Basic checks on QueuedPromise", () => {
+  let customQueue: PromiseQueue;
+  let QueuedPromise: QueuedPromiseFactory;
 
-    let customQueue: PromiseQueue;
-    let QueuedPromise: QueuedPromiseFactory;
+  beforeEach(() => {
+    customQueue = new PromiseQueue(1);
+    QueuedPromise = customQueue.QueuedPromise;
+  });
 
-    beforeEach(() => {
-        customQueue = new PromiseQueue(1);
-        QueuedPromise = customQueue.QueuedPromise;
-    })
-
-
-    it('Checking when rejected that only catch is fired', () => {
-
-        const promise = QueuedPromise((resolve, reject) => {
-            reject(6);
-        });
-
-
-        return expect(promise).rejects.toEqual(6);
+  it("Checking when rejected that only catch is fired", () => {
+    const promise = QueuedPromise((resolve, reject) => {
+      reject(6);
     });
 
-    it('catch() works', () => {
+    return expect(promise).rejects.toEqual(6);
+  });
 
-        const promise = QueuedPromise((resolve, reject) => {
-            reject(5);
-        });
-
-        return expect(promise).rejects.toEqual(5);
+  it("catch() works", () => {
+    const promise = QueuedPromise((resolve, reject) => {
+      reject(5);
     });
+
+    return expect(promise).rejects.toEqual(5);
+  });
 });
 
-describe('Basic checks on QueuedTask', () => {
+describe("Basic checks on QueuedTask", () => {
+  let customQueue: PromiseQueue;
+  let QueuedTask: QueuedTaskFactory;
 
-    let customQueue: PromiseQueue;
-    let QueuedTask: QueuedTaskFactory;
+  beforeEach(() => {
+    customQueue = new PromiseQueue(1);
+    QueuedTask = customQueue.QueuedTask;
+  });
 
-    beforeEach(() => {
-        customQueue = new PromiseQueue(1);
-        QueuedTask = customQueue.QueuedTask;
-    })
-
-
-    it('Checking when rejected that only catch is fired', (done) => {
-
-        const promise = QueuedTask((resolve, reject) => {
-            reject(6);
-        });
-
-        let hasRunThen = false;
-        let hasRunCatch = false;
-        promise.then(() => {
-            hasRunThen = true;
-        }).catch(() => {
-            hasRunCatch = true;
-        }).finally(() => {
-            expect(hasRunThen).toBe(false);
-            expect(hasRunCatch).toBe(true);
-            done();
-        });
+  it("Checking when rejected that only catch is fired", (done) => {
+    const promise = QueuedTask((resolve, reject) => {
+      reject(6);
     });
 
-    it('catch() works', (done) => {
+    let hasRunThen = false;
+    let hasRunCatch = false;
+    promise
+      .then(() => {
+        hasRunThen = true;
+      })
+      .catch(() => {
+        hasRunCatch = true;
+      })
+      .finally(() => {
+        expect(hasRunThen).toBe(false);
+        expect(hasRunCatch).toBe(true);
+        done();
+      });
+  });
 
-        const returnedValues: any[] = [];
-        const promise = QueuedTask((resolve, reject) => {
-            reject(5);
-        });
-
-        promise.catch((e: any) => {
-            returnedValues.push(e);
-            returnedValues.push(2);
-        }).finally(() => {
-            expect(returnedValues).toMatchObject([5,2]);
-            done();
-        });
+  it("catch() works", (done) => {
+    const returnedValues: any[] = [];
+    const promise = QueuedTask((resolve, reject) => {
+      reject(5);
     });
+
+    promise
+      .catch((e: any) => {
+        returnedValues.push(e);
+        returnedValues.push(2);
+      })
+      .finally(() => {
+        expect(returnedValues).toMatchObject([5, 2]);
+        done();
+      });
+  });
 });
 
-describe('Promise order check', () => {
+describe("Promise order check", () => {
+  it("Single queue (Forces set order)", (done) => {
+    const customQueue = new PromiseQueue(1);
+    const QueuedPromise = customQueue.QueuedPromise;
 
-    it('Single queue (Forces set order)', (done) => {
-        const customQueue = new PromiseQueue(1);
-        const QueuedPromise = customQueue.QueuedPromise;
+    const returnedValues: any[] = [];
 
-        const returnedValues: any[] = [];
-
-        QueuedPromise((resolve: () => void) => {
-            returnedValues.push(1);
-            resolve();
-        });
-
-        QueuedPromise((resolve: () => void) => {
-            setTimeout(() => {
-                returnedValues.push(2);
-                resolve();
-            }, 10);
-        });
-
-        QueuedPromise((resolve: () => void) => {
-            returnedValues.push(3);
-            resolve();
-        });
-
-        customQueue.on('finished', () => {
-            expect(returnedValues).toMatchObject([1,2,3]);
-            done();
-        });
-
+    QueuedPromise((resolve: () => void) => {
+      returnedValues.push(1);
+      resolve();
     });
 
-    it('Double queue', (done) => {
-
-        const customQueue = new PromiseQueue(2);
-        const QueuedPromise = customQueue.QueuedPromise;
-
-        const returnedValues: any[] = [];
-
-        QueuedPromise((resolve: () => void) => {
-            returnedValues.push(1);
-            resolve();
-        });
-        QueuedPromise((resolve: () => void) => {
-            setTimeout(() => {
-                returnedValues.push(2);
-                resolve();
-            }, 10);
-        });
-        QueuedPromise((resolve: () => void) => {
-            returnedValues.push(3);
-            resolve();
-        });
-
-        customQueue.on('finished', () => {
-            expect(returnedValues).toMatchObject([1,3,2]);
-            done();
-        });
-
+    QueuedPromise((resolve: () => void) => {
+      setTimeout(() => {
+        returnedValues.push(2);
+        resolve();
+      }, 10);
     });
 
-    it('Emit finished event', (done) => {
-
-        const customQueue = new PromiseQueue(2);
-        const QueuedPromise = customQueue.QueuedPromise;
-
-        const returnedValues: any[] = [];
-
-        customQueue.on('finished', () => {
-            expect(returnedValues).toMatchObject([1,2,3]);
-            done();
-        });
-
-        QueuedPromise((resolve: () => void) => {
-            returnedValues.push(1);
-            resolve();
-        });
-        QueuedPromise((resolve: () => void) => {
-            returnedValues.push(2);
-            resolve();
-        });
-        QueuedPromise((resolve: () => void) => {
-            returnedValues.push(3);
-            resolve();
-        });
+    QueuedPromise((resolve: () => void) => {
+      returnedValues.push(3);
+      resolve();
     });
 
-    it('Pause Queue', (done) => {
+    customQueue.on("finished", () => {
+      expect(returnedValues).toMatchObject([1, 2, 3]);
+      done();
+    });
+  });
 
-        const customQueue = new PromiseQueue(2);
-        const QueuedPromise = customQueue.QueuedPromise;
+  it("Double queue", (done) => {
+    const customQueue = new PromiseQueue(2);
+    const QueuedPromise = customQueue.QueuedPromise;
 
-        customQueue.pause();
+    const returnedValues: any[] = [];
 
-        const returnedValues: any[] = [];
-
-        QueuedPromise((resolve: () => void) => {
-            returnedValues.push(1);
-            resolve();
-        });
-        QueuedPromise((resolve: () => void) => {
-            setTimeout(() => {
-                returnedValues.push(2);
-                resolve();
-            }, 5);
-        });
-        QueuedPromise((resolve: () => void) => {
-            returnedValues.push(3);
-            resolve();
-        });
-
-        setTimeout(() => {
-            expect(returnedValues).toMatchObject([]);
-        }, 10);
-
-        setTimeout(() => {
-            expect(returnedValues).toMatchObject([]);
-            customQueue.resume();
-        }, 20);
-
-
-
-        customQueue.on('finished', () => {
-            expect(returnedValues).toMatchObject([1,3,2]);
-            done();
-        });
+    QueuedPromise((resolve: () => void) => {
+      returnedValues.push(1);
+      resolve();
+    });
+    QueuedPromise((resolve: () => void) => {
+      setTimeout(() => {
+        returnedValues.push(2);
+        resolve();
+      }, 10);
+    });
+    QueuedPromise((resolve: () => void) => {
+      returnedValues.push(3);
+      resolve();
     });
 
+    customQueue.on("finished", () => {
+      expect(returnedValues).toMatchObject([1, 3, 2]);
+      done();
+    });
+  });
+
+  it("Emit finished event", (done) => {
+    const customQueue = new PromiseQueue(2);
+    const QueuedPromise = customQueue.QueuedPromise;
+
+    const returnedValues: any[] = [];
+
+    customQueue.on("finished", () => {
+      expect(returnedValues).toMatchObject([1, 2, 3]);
+      done();
+    });
+
+    QueuedPromise((resolve: () => void) => {
+      returnedValues.push(1);
+      resolve();
+    });
+    QueuedPromise((resolve: () => void) => {
+      returnedValues.push(2);
+      resolve();
+    });
+    QueuedPromise((resolve: () => void) => {
+      returnedValues.push(3);
+      resolve();
+    });
+  });
+
+  it("Pause Queue", (done) => {
+    const customQueue = new PromiseQueue(2);
+    const QueuedPromise = customQueue.QueuedPromise;
+
+    customQueue.pause();
+
+    const returnedValues: any[] = [];
+
+    QueuedPromise((resolve: () => void) => {
+      returnedValues.push(1);
+      resolve();
+    });
+    QueuedPromise((resolve: () => void) => {
+      setTimeout(() => {
+        returnedValues.push(2);
+        resolve();
+      }, 5);
+    });
+    QueuedPromise((resolve: () => void) => {
+      returnedValues.push(3);
+      resolve();
+    });
+
+    setTimeout(() => {
+      expect(returnedValues).toMatchObject([]);
+    }, 10);
+
+    setTimeout(() => {
+      expect(returnedValues).toMatchObject([]);
+      customQueue.resume();
+    }, 20);
+
+    customQueue.on("finished", () => {
+      expect(returnedValues).toMatchObject([1, 3, 2]);
+      done();
+    });
+  });
 });
 
-describe('Works with promise functions', () => {
+describe("Works with promise functions", () => {
+  it("Resolves", async () => {
+    const customQueue = new PromiseQueue(1);
+    const QueuedPromise = customQueue.QueuedPromise;
 
-    it('Resolves', async () => {
-        const customQueue = new PromiseQueue(1);
-        const QueuedPromise = customQueue.QueuedPromise;
-
-        const testPromise = QueuedPromise((resolve: (text: string) => void) => {
-            resolve("Value has returned");
-        });
-
-        const testPromise2 = QueuedPromise((resolve: (text: string) => void) => {
-            resolve("Here is another");
-        });
-
-        const testPromise3 = QueuedPromise((resolve: (text: string) => void) => {
-            resolve("My name is steve");
-        });
-
-        await expect(testPromise).resolves.toEqual("Value has returned");
-        await expect(testPromise2).resolves.toEqual("Here is another");
-        await expect(testPromise3).resolves.toEqual("My name is steve");
+    const testPromise = QueuedPromise((resolve: (text: string) => void) => {
+      resolve("Value has returned");
     });
 
-    it('Works with promise all', async () => {
-        const customQueue = new PromiseQueue(1);
-        const QueuedPromise = customQueue.QueuedPromise;
-
-        const testPromise = QueuedPromise((resolve: (text: string) => void) => {
-            resolve("test1");
-        });
-
-        const testPromise2 = QueuedPromise((resolve: (text: string) => void) => {
-            resolve("test2");
-        });
-
-        const testPromise3 = QueuedPromise((resolve: (text: string) => void) => {
-            resolve("test3");
-        });
-
-        await expect(Promise.all([testPromise, testPromise2, testPromise3])).resolves.toEqual(["test1","test2","test3"]);
+    const testPromise2 = QueuedPromise((resolve: (text: string) => void) => {
+      resolve("Here is another");
     });
 
-    it('Works with await', async () => {
-        const customQueue = new PromiseQueue(1);
-        const QueuedPromise = customQueue.QueuedPromise;
-
-        const result = await QueuedPromise((resolve: (text: string) => void) => {
-            resolve("Await test");
-        });
-
-        expect(result).toEqual("Await test");
+    const testPromise3 = QueuedPromise((resolve: (text: string) => void) => {
+      resolve("My name is steve");
     });
 
+    await expect(testPromise).resolves.toEqual("Value has returned");
+    await expect(testPromise2).resolves.toEqual("Here is another");
+    await expect(testPromise3).resolves.toEqual("My name is steve");
+  });
 
+  it("Works with promise all", async () => {
+    const customQueue = new PromiseQueue(1);
+    const QueuedPromise = customQueue.QueuedPromise;
+
+    const testPromise = QueuedPromise((resolve: (text: string) => void) => {
+      resolve("test1");
+    });
+
+    const testPromise2 = QueuedPromise((resolve: (text: string) => void) => {
+      resolve("test2");
+    });
+
+    const testPromise3 = QueuedPromise((resolve: (text: string) => void) => {
+      resolve("test3");
+    });
+
+    await expect(Promise.all([testPromise, testPromise2, testPromise3])).resolves.toEqual(["test1", "test2", "test3"]);
+  });
+
+  it("Works with await", async () => {
+    const customQueue = new PromiseQueue(1);
+    const QueuedPromise = customQueue.QueuedPromise;
+
+    const result = await QueuedPromise((resolve: (text: string) => void) => {
+      resolve("Await test");
+    });
+
+    expect(result).toEqual("Await test");
+  });
 });
